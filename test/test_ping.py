1a993a47f22bfe676f38f4cf7aee3a0a7f707b76
Tomás Hernández <tom.jes23@gmail.com>
2025-10-01 12:12:48 -0300

    Creado un test erroneo para pruebas de workflow

--- test/test_ping.py
+++ test/test_ping.py
@@ -1,4 +1,3 @@
-import pytest
 
 def test_ping(client):
     for _ in range(5):
@@ -7,3 +6,6 @@ def test_ping(client):
     resp = client.get("/api/ping/")
     assert resp.status_code == 429
     assert b"Too Many Request" in resp.data
+
+def test_error():
+    assert 1 == 2
